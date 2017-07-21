53c15af5a45a912e5956bf002b62bd75ea2a05a5
Eagle19243 <eagle19243@gmail.com>
2017-07-20 21:14:46 -0700

    b1

---
+++ index.php
@@ -0,0 +1,156 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Proof of concept</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0">
+        
+        <link rel="stylesheet" type="text/css" href="public/vendor/slick/slick.css">
+        <link rel="stylesheet" type="text/css" href="public/vendor/slick/slick-theme.css"/>
+        <link rel="stylesheet" type="text/css" href="public/css/style.min.css">
+        
+        <script type="text/javascript" src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
+        <script type="text/javascript" src="public/vendor/slick/slick.min.js"></script>
+        <script type="text/javascript" src="public/js/index.min.js"></script>
+
+        <script type="text/javascript" src="public/js/jquery.event.move.js"></script>
+        <script type="text/javascript" src="public/js/jquery.event.swipe.js"></script>
+
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.2.0/js/tether.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/js/bootstrap.min.js"></script>
+        
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/wow/1.1.2/wow.js"></script>
+        <script src="public/js/scripts.js"></script>
+    </head>
+    <body>
+        <div id="banner">
+            <img id="start" src="public/img/start.png"/>
+        </div>
+        <div id="intro_video">
+            <video poster="https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/polina.jpg" id="bgvid" playsinline>
+                <!-- WCAG general accessibility recommendation is that media such as background video play through only once. Loop turned on for the purposes of illustration; if removed, the end of the video will fade in the same way created by pressing the "Pause" button  -->
+                <source src="http://vjs.zencdn.net/v/oceans.mp4" type="video/mp4">
+            </video>
+            <img id="skip" src="public/img/skip.png"/>
+        </div>
+        <div id="main">
+            <div id="preview" class="slider-nav">
+                <div>
+                    <img src="https://unsplash.it/1400/600?image=622" class="preview-img">
+                </div>
+                <div>
+                    <img src="https://unsplash.it/1400/600?image=114" class="preview-img">
+                </div>
+                <div>
+                    <img src="https://unsplash.it/1400/600?image=315" class="preview-img">
+                </div>
+                <div>
+                    <img src="https://unsplash.it/1400/600?image=622" class="preview-img">
+                </div>
+                <div>
+                    <img src="https://unsplash.it/1400/600?image=401" class="preview-img">
+                </div>
+            </div>
+            <div id="detail" class="slider-for">
+                <div>
+                    <div class="detail-container">
+                        <div class="item-title">
+                            <span>How To Hack Someone Off A Segway Scooter In 20 Seconds</span>
+                        </div>
+                        <img src="https://unsplash.it/1400/600?image=622" class= "item-img"/>
+                        <div class="item-description">
+                            <p>
+                                Any hoverboard rider&nbsp;should be concerned about their physical wellbeing. Thanks to digital weaknesses in Segway's hands-free Ninebot miniPRO scooter, they've got more reason to worry. The&nbsp;flaws made it possible to take almost complete control of the self-balancing vehicle and, potentially, knock off anyone aboard with some fairly basic hacks, researchers said today.
+                                <br>
+                                <br>
+                                Attacks could be carried out with&nbsp;just 20 seconds of continuous Bluetooth connection to a Segway hoverboard, said IOActive researcher Thomas Kilbride. "It may be sped up using other means," he told <em>Forbes</em>. "It's a little bit alarming." Not only would it be possible to kill the motor mid-drive, but it'd be simple to steal a miniPRO too, as seen in his demonstration video below.
+                            </p>
+                            <p>
+                                In his proof-of-concept hack, he started using the official Ninebot smartphone app that's used to control certain functions of the vehicle. That let him determine the location of nearby riders. For whatever reason, a now-removed feature in the app saw GPS locations of each scooter user made available to anyone in a given area. As Kilbride wrote in an <a href="http://blog.ioactive.com/2017/07/multiple-critical-vulnerabilities-found.html" target="_blank">advisory</a> released today: "Each riders' location was published and publicly available, which makes weaponization of an exploit much easier for an attacker."
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div class="detail-container">
+                        <div class="item-title">
+                            <span>The Sad Drowning Of Steve The Robot And The Future Of Robotic Rights</span>
+                        </div>
+                        <img src="https://unsplash.it/1400/600?image=114" class= "item-img"/>
+                        <div class="item-description">
+                            <p>
+                                On Monday a robotic security guard here in Washington was found <a href="http://www.cnn.com/2017/07/18/us/security-robot-drown-trnd/index.html" target="_blank">drowned</a> in a fountain at his workplace after apparently failing to see a set of stairs leading down to it and falling to his watery demise. In this case, no foul play is suspected and it is believed the Knightscope robot simply failed to see the stairs, but the incident raises fascinating questions about robotic rights and the future of our mechanized overlords.
+                            </p>
+                            <p>
+                                I actually met Steve, as the robot was affectionately named, for the first time on my nightly walk through Georgetown just a few hours before his death. He was sitting happily on what appeared to be a charging mat just around the corner from his ultimate resting place,&nbsp;all lit up in his blue LED lights, the number 42 stamped on the bottom of his case and the words “Designed and built in Silicon Valley” proudly emblazoned on his chest.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div class="detail-container">
+                        <div class="item-title">
+                            <span>Recruiting Software Firm Lever Raises $30M For Effort To Transform Hiring</span>
+                        </div>
+                        <img src="https://unsplash.it/1400/600?image=315" class= "item-img"/>
+                        <div class="item-description">
+                            <div class="item-description">
+                                <p>
+                                    Sarah Nahm cofounded <a href="https://www.lever.co" target="_blank">Lever </a>five years ago to rethink recruiting for the digital age. The San Francisco-based recruiting software company helps its customers not only track resumes, but also identify and cultivate people who might not be actively job-hunting.
+                                </p>
+                                <p>
+                                    As the recruiting space becomes increasingly competitive, Lever has amassed&nbsp;a significant war chest. Today, it said that it had raised an additional $30 million in venture financing from Adams Street Partners, Matrix Partners and Scale Partners, bringing its total funding to $62 million. It could raise as much as $10 million more before officially closing the round. That gives Lever, which FORBES estimates has revenues above $20 million, the cash to do more R&amp;D and to add new analytical functions to its software to meet the needs of highly complex, global corporations. Lever’s more than 1,300 customers include Netflix, KPMG, <a href="https://www.forbes.com/sites/alexkonrad/2016/01/26/lever-raises-20-million-to-help-lyft-hire/#230673975fa4" target="_self">Lyft </a>and Cirque du Soleil. “We’ve tried to rethink what has been a stale category,” Nahm says.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div class="detail-container">
+                        <div class="item-title">
+                            <span>The House Budget Panel's Fiscal Plan Could Open The Door To A Big Tax Bill, If It Survives</span>
+                        </div>
+                        <img src="https://unsplash.it/1400/600?image=622" class= "item-img"/>
+                        <div class="item-description">
+                            <p>
+                                Hours after the collapse of the Senate GOP leadership’s health plan, Republicans began to move on. The House began its efforts to write a tax bill when the Budget Committee released a draft budget resolution. The goal: To make that fiscal plan the vehicle for a major tax measure that could pass Congress with no Democratic votes.
+                            </p>
+                            <p>
+                                However, the House Budget Committee’s <a href="https://budget.house.gov/budgets/fy18/" target="_blank">fiscal blueprint</a> suffers from many of the same flaws as the congressional effort to replace the Affordable Care Act. It attempts to paper over, but does not really address, deep divisions within the Republican caucus over both taxes and spending. And, like the ACA replacement bills, this draft faces an uphill battle in the House and has little or no chance of being approved by the Senate where, as we have learned, GOP legislative control is tenuous at best.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div class="detail-container">
+                        <div class="item-title">
+                            <span>Baidu's Apollo Lifts Off: Microsoft Taking Open-Source Robocar Project Global</span>
+                        </div>
+                        <img src="https://unsplash.it/1400/600?image=401" class= "item-img"/>
+                        <div class="item-description">
+                            <p> 
+                                Fast on the heels of Baidu’s announcement that more than 50 companies have joined its open-source “Project Apollo” initiative to perfect driverless car technology, Microsoft is enlisting&nbsp;to help the&nbsp;China-centric effort&nbsp;expand into the U.S. and Europe.
+                            </p>
+                            <p>
+                                Under the terms of the partnership between the Chinese and U.S. tech giants, <a href="https://azure.microsoft.com/en-us/?v=17.14" target="_blank">Microsoft Azure</a> will serve as Apollo’s global cloud platform. The two will also find opportunities to provide connected vehicle services for autonomous driving, the companies said.
+                            </p>
+                            <p>
+                                Apollo “has gained a lot of momentum since April and we have gained a lot of partners around the world. … two thirds from within China, one third from outside of China,” <a href="http://ir.baidu.com/phoenix.zhtml?c=188488&amp;p=irol-govBio&amp;ID=239174" target="_blank">Ya-Qin Zhang</a>, Baidu’s president of technology, emerging business and global business operations, told <em>Forbes</em> in an interview. “We want to extend this to the world and believe this whole technology can be shared by China, and that's why we partnered with Microsoft.”
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div id="chat">
+            <div id="control-box">
+                <div id="chat-title">
+                    We can chat here!
+                </div>
+            </div>
+            <div id="widget-box">
+            </div>
+        </div>
+    </body>
+</html>
---
+++ public/js/index.js
@@ -0,0 +1,235 @@
+var swipeDirection = 'none';
+var detailScrollState = 'top';
+var isScrollAtTop = true;
+var isScrollAtBottom = false;
+
+$(document).ready(function(){
+    var vid = document.getElementById("bgvid");
+
+    if (window.matchMedia('(prefers-reduced-motion)').matches) {
+        vid.removeAttribute("autoplay");
+        vid.pause();
+        pauseButton.innerHTML = "Paused";
+    }
+
+    function vidFade() {
+        vid.classList.add("stopfade");
+    }
+
+    $("#start").click(function(){
+        $("#start").fadeOut(1000, function(){
+            $("#banner").hide();
+            $("#intro_video").show(500);
+            vid.play();
+        });
+    });
+
+    $("#skip").click(function(){
+        vid.pause();
+        vidFade();
+        $("#intro_video").hide(function(){
+            $("#main").show();
+            $('.slider-nav').slick('slickGoTo', 0, false);
+            $('.slider-nav').slick('slickSetOption', 'speed', 300, true);
+        });
+    });
+
+    vid.addEventListener('ended', function()
+    {
+        vid.pause();
+        vidFade();
+        $("#intro_video").hide(function(){
+            $("#main").show();
+            $('.slider-nav').slick('slickGoTo', 0, false);
+            $('.slider-nav').slick('slickSetOption', 'speed', 300, true);
+        });
+    });
+    
+
+    $('.slider-for').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        infinite: false,
+        asNavFor: '.slider-nav'
+    });
+
+    $('.slider-nav').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        asNavFor: '.slider-for',
+        arrows: true,
+        dots: true,
+        infinite: false,
+        speed: 0,
+        focusOnSelect: true
+    });
+
+    $("#preview")
+    .on('edge', function(event, slick, direction) {
+        if (direction == 'left') {
+            $("#main").hide(function() {
+                $("#chat").show();
+                window.replySDK = (function(d, s, id) {
+                    var o = window.replySDK || {};var js,
+                    fjs = d.getElementsByTagName(s)[0];if (d.getElementById(id)) return;
+                    js = d.createElement(s); js.id = id;
+                    js.src = "https://d1fidecqhnmd5.cloudfront.net/prod/v1.3.1/dist/js/reply-sdk.js";
+                    fjs.parentNode.insertBefore(js, fjs);
+                    o.isLoaded=false;o._e = [];o.load = function(f) { o._e.push(f);};
+                    js.onload = function () { replySDK.init( widgetSettings ); };return o;
+                }(document, 'script', 'reply-sdk-js'));
+
+                replySDK.load(function () {
+                    $(".Close").click(function(){
+                        replySDK.close();
+                    });
+                    $(".Open").click(function(){
+                        replySDK.open();
+                    });
+                    $(".Hide").click(function(){
+                        replySDK.hide();
+                    });
+                    $(".Show").click(function(){
+                        replySDK.show();
+                    });
+                    
+                    replySDK.on("message:received", function(message){
+                        console.log("on message:received", message);
+                    });
+
+                    replySDK.on("message:sent", function(message){
+                        console.log("on message:sent", message);
+                    });
+
+                    replySDK.on("widget:shown", function(){
+                        console.log("on widget:shown");
+                    });
+                    
+                    replySDK.on("widget:hidden", function(){
+                        console.log("on widget:hidden");
+                    });
+                    
+                    replySDK.on("widget:opened", function(){
+                        console.log("on widget:opened");
+                    });
+                    
+                    replySDK.on("widget:closed", function(){
+                        console.log("on widget:closed");
+                    });         
+                });
+            });
+        }
+    })
+    .on('movestart', function(e) {
+        $(".slider-nav").css("transition", "0");
+        if(Math.abs(e.distX) < Math.abs(e.distY)) {
+            if (e.distY > 0) {
+                swipeDirection = 'down';
+            }
+            else {
+                swipeDirection = 'up';
+            }
+            $('.slider-nav').slick('slickSetOption', 'draggable', false, true);
+        }
+        else {
+            if (e.distX > 0) {
+                swipeDirection = 'right';
+            }
+            else {
+                swipeDirection = 'left';
+            }
+        }
+    })
+    .on('move', function(e) {
+        if (swipeDirection == 'up') {
+            document.getElementById("preview").style.top = e.distY + 'px';
+            if (document.getElementById("preview").getBoundingClientRect().top > 0) {
+                document.getElementById("preview").style.top = '0px';
+            }
+        }
+    })
+    .on('moveend', function(e) {
+        if (swipeDirection == 'up') {
+            $(".slider-nav").css("transition", "0.3s");
+            if (Math.abs(e.distY) > $('.slider-nav').height() * 0.3) {
+                document.getElementById("preview").style.top = -$('.slider-nav').height() + 'px';
+            }
+            else {
+                document.getElementById("preview").style.top = '0px';
+            }
+        }
+        $('.slider-nav').slick('slickSetOption', 'draggable', true, true);
+        swipeDirection = 'none';
+    });
+
+    $("#detail")
+    .on('movestart', function(e) {
+        if (detailScrollState == 'top') {
+            isScrollAtTop = true;
+            isScrollAtBottom = false;
+        }
+        else if (detailScrollState == 'bottom'){
+            isScrollAtTop = false;
+            isScrollAtBottom = true;
+        }
+        else {
+            isScrollAtTop = false;
+            isScrollAtBottom = false;
+        }
+
+        if(Math.abs(e.distX) < Math.abs(e.distY)) {
+            $('.slider-for').slick('slickSetOption', 'draggable', false, true);
+        }
+    })
+    .on('moveend', function(e) {
+        $('.slider-nav').slick('slickSetOption', 'draggable', true, true);
+    })
+    .on('swipedown', function(e) {
+        if (isScrollAtTop) {
+            document.getElementById("preview").style.top = 0;
+        }
+    })
+    .on('swipeup', function(e) {
+        if (isScrollAtBottom) {
+            document.getElementById("preview").style.top = 0;
+        }
+    });
+
+    $('.detail-container').on('scroll', function() {
+        if($(this).scrollTop() == 0) {
+            detailScrollState = 'top';
+        }
+        else if($(this).scrollTop() + $(this).innerHeight() >= $(this)[0].scrollHeight) {
+            detailScrollState = 'bottom';
+        }
+        else {
+            detailScrollState = 'middle';
+        }
+    });
+
+    var widgetSettings = {
+        channelUuid: "e600b3b7-11d6-4b97-a79c-6ab3fed75a86",
+        style: "embed", // "fixed", "embed" or "fullpage". Open the sample page for all the configuration paramaters
+        appendTo: "#widget-box",
+        width:"100%",
+        height: "100%",
+        welcomeMessage : {
+            text : "Hi, I'm the Activity Finder Bot! I see you're looking for fun activities to do at the Old Port. Let's find the perfect one for you!\nWhat do you say?",
+            buttons:[
+                {
+                    type :"postback",
+                    title : "Get Started",
+                    payload: "activity_finder_start"
+                }
+            ]
+        },
+    };
+
+    $("#main").show();
+    $(".preview-img").css("height", $(".slider-nav").css("height"));
+    $(".preview-img").css("width", $(".slider-nav").css("width"));
+    $(".detail-container").css("height", $(".slider-nav").css("height"));
+    $(".detail-container").css("width", $(".slider-nav").css("width"));
+    $("#main").hide();
+});
